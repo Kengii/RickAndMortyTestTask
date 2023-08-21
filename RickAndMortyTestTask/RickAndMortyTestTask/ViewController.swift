c74c5412d5b130c1c1635eced31f9864670d1e63
Kengii <kengii9431@gmail.com>
2023-08-22 00:23:12 +0300

    ProjectCreated

---
+++ RickAndMortyTestTask/RickAndMortyTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyTestTask
+//
+//  Created by Владимир Данилович on 22.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
